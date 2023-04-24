03b903c8e5b46961a7231d507831f4a288906c35
awinterlee <alexwinterlee@gmail.com>
2023-04-24 16:34:04 -0400

    view functions for customers done. get , get, put, post, and delete

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import list_salespeople, detail_salespeople
+from .views import list_salespeople, detail_salespeople, list_customers, detail_customer
 
 urlpatterns = [
     path('salespeople/', list_salespeople, name='list_salespeople'),
     path('salespeople/<int:pk>', detail_salespeople, name="detail_salespeople"),
-    
+    path('customers/', list_customers, name='list_customers'),
+    path('customers/<int:pk>', detail_customer, name='detail_customer'),
+
 ]
--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,5 +1,5 @@
 from django.views.decorators.http import require_http_methods
-from .models import Salesperson
+from .models import Salesperson, Customer
 import json
 from django.http import JsonResponse
 from common.json import ModelEncoder
@@ -19,6 +19,24 @@ class SalespersonDetailEncoder(ModelEncoder):
         "first_name",
         "last_name",
         "employee_id",
+    ]
+
+
+class CustomerListEncoder(ModelEncoder):
+    model = Customer
+    properties = [
+        "first_name",
+        "last_name",
+    ]
+
+
+class CustomerDetailEncoder(ModelEncoder):
+    model = Customer
+    properties = [
+        "first_name",
+        'last_name',
+        'address',
+        'phone_number',
     ]
 
 
@@ -58,4 +76,43 @@ def detail_salespeople(request, pk):
         salesperson = Salesperson.objects.get(id=pk)
         return JsonResponse(
             salesperson, encoder=SalespersonDetailEncoder, safe=False
+        )
+
+
+@require_http_methods(['GET', 'POST'])
+def list_customers(request):
+    if request.method == 'GET':
+        customers = Customer.objects.all()
+        return JsonResponse(
+            {'customers': customers},
+            encoder=CustomerListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        customer = Customer.objects.create(**content)
+        return JsonResponse(
+            customer,
+            encoder=CustomerDetailEncoder,
+            safe=False
+        )
+
+
+@require_http_methods(['DELETE', 'GET', 'PUT'])
+def detail_customer(request, pk):
+    if request.method == 'GET':
+        customer = Customer.objects.get(id=pk)
+        return JsonResponse(
+            {'customer': customer}, encoder=CustomerDetailEncoder
+        )
+    elif request.method == 'DELETE':
+        count, _ = Customer.objects.filter(id=pk).delete()
+        return JsonResponse(
+            {"deleted": count > 0}
+        )
+    else:
+        content = json.loads(request.body)
+        Customer.objects.filter(id=pk).update(**content)
+        customer = Customer.objects.get(id=pk)
+        return JsonResponse(
+            customer, encoder=CustomerDetailEncoder, safe=False
         )
